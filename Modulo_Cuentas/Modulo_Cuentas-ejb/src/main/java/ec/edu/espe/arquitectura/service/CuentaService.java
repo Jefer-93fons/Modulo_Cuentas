a02d83726c853daf4ae1c95582465aacd8edb2ab
juan.sebas.at@gmail.com <juan.sebas.at@gmail.com>
2018-11-10 12:41:05 -0500

    Corrección de conflictos

--- Modulo_Cuentas/Modulo_Cuentas-ejb/src/main/java/ec/edu/espe/arquitectura/service/CuentaService.java
+++ Modulo_Cuentas/Modulo_Cuentas-ejb/src/main/java/ec/edu/espe/arquitectura/service/CuentaService.java
@@ -5,7 +5,10 @@
  */
 package ec.edu.espe.arquitectura.service;
 
+
+import ec.edu.espe.arquitectura.dao.ClienteFacade;
 import ec.edu.espe.arquitectura.dao.CuentaFacade;
+import ec.edu.espe.arquitectura.model.Cliente;
 import ec.edu.espe.arquitectura.model.Cuenta;
 import java.util.List;
 import javax.ejb.EJB;
@@ -14,25 +17,36 @@ import javax.ejb.Stateless;
 
 /**
  *
- * @author DAVID
+ * @author Jefferson
  */
 @Stateless
 @LocalBean
 public class CuentaService {
-
     @EJB
     private CuentaFacade cuentaFacade;
+    private ClienteFacade clienteFacade;
     
-        public List<Cuenta> obtenerTodos(){
+    public List<Cuenta> obtenerTodos(){
         return this.cuentaFacade.findAll();
     }
+    public Cuenta obtenerPorCodigo(Integer codigo) {
+        return this.cuentaFacade.find(codigo);
+    }
     
+    public Cliente obtenerCliente (Integer codigo) {
+        return this.clienteFacade.find(codigo);
+    }
     public void crear(Cuenta cuenta){
         this.cuentaFacade.create(cuenta);
     }
     
     public void modificar(Cuenta cuenta){
         this.cuentaFacade.edit(cuenta);
+    }
+    
+    public void eliminar(Integer codigo){
+        Cuenta cuenta = this.cuentaFacade.find(codigo);
+        this.cuentaFacade.remove(cuenta);
     }
 
 }
