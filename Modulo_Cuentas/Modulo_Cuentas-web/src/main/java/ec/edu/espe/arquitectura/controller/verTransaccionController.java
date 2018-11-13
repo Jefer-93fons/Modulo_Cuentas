ad81adc129191f3139328379fd74986cc8d37314
PC\Usuario <jefer_93fons@hotmail.es>
2018-11-13 17:18:07 -0500

    Filtro por fechas Transacciones

--- Modulo_Cuentas/Modulo_Cuentas-web/src/main/java/ec/edu/espe/arquitectura/controller/verTransaccionController.java
+++ Modulo_Cuentas/Modulo_Cuentas-web/src/main/java/ec/edu/espe/arquitectura/controller/verTransaccionController.java
@@ -12,8 +12,14 @@ import ec.edu.espe.arquitectura.model.Producto;
 import ec.edu.espe.arquitectura.model.Transaccion;
 import ec.edu.espe.arquitectura.service.CuentaService;
 import ec.edu.espe.arquitectura.service.TransaccionService;
+import ec.edu.espe.arquitectura.web.util.FacesUtil;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
@@ -31,6 +37,11 @@ public class verTransaccionController extends BaseController implements Serializ
     private int cuentaDigitada;
     private Cuenta cuenta;
     private List<Cuenta> cuentas;
+    private String mes;
+    private List<List<String>>mesesList;
+    private String anio;
+    private List<String> anios;
+    
     @Inject
     private TransaccionService transaccionService;
     @Inject
@@ -42,6 +53,26 @@ public class verTransaccionController extends BaseController implements Serializ
     @PostConstruct
     public void init() {
         resetData();
+        
+        mesesList = new ArrayList<List<String>>();
+        anios = new ArrayList<String>();
+        
+        String meses[] = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+        
+        for(int i=1; i<13; i++){
+            List<String> mesPosicion = new ArrayList<String>();
+            if(i<10){
+                mesPosicion.add("0"+i);
+            }else{
+                mesPosicion.add(String.valueOf(i));
+            }
+            mesPosicion.add(meses[i-1]);
+            mesesList.add(mesPosicion);
+        }
+    
+        anios.add("2016");
+        anios.add("2017");
+        anios.add("2018");
     }
 
     @Override
@@ -110,6 +141,39 @@ public class verTransaccionController extends BaseController implements Serializ
         this.transaccionService = transaccionService;
     }
 
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public List<List<String>> getMesesList() {
+        return mesesList;
+    }
+
+    public void setMesesList(List<List<String>> mesesList) {
+        this.mesesList = mesesList;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+
+    public List<String> getAnios() {
+        return anios;
+    }
+
+    public void setAnios(List<String> anios) {
+        this.anios = anios;
+    }
+
+    
     public void resetData() {
         this.cuentaDigitada = 0;
         this.cuentas = cuentaService.obtenerTodos();
@@ -118,7 +182,21 @@ public class verTransaccionController extends BaseController implements Serializ
         this.cuenta.setIdProducto(new Producto());
     }
     
-    public void buscar(){
-        
+    public void buscarTransaccion() throws ParseException {
+        if(mes!=null && anio!=null){
+            SimpleDateFormat inputFecha = new SimpleDateFormat("MM/yyyy");  
+            Date fechabusqueda = inputFecha.parse(this.mes+"/"+this.anio);
+            Date fechainput;
+            List<Transaccion> transaccionSel = new ArrayList<Transaccion>();
+
+            for (Transaccion trans : transacciones){
+                fechainput =  inputFecha.parse(inputFecha.format(trans.getFechaTransaccion()));
+                 if(fechainput.equals(fechabusqueda)){
+                     transaccionSel.add(trans);
+                }          
+                
+            }
+            transacciones = transaccionSel;
+        }
     }
 }
